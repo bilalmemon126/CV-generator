c4b5b7e92f44df22487b634d2b0c2c0d75e9e8bb
bilalmemon126 <bilalm3m9n@gmail.com>
2025-02-03 01:20:38 +0500

    some changes

--- script.js
+++ script.js
@@ -29,139 +29,140 @@ btn.addEventListener("click", function(){
     
         objective = document.getElementById("objective").value;
         getObjective = document.getElementById("getObjective").innerHTML = objective;
-    
+
+
         qualificationUL = document.getElementById("qualificationUL");
         qualificationLI = document.createElement('li');
-        qualificationLI.id = "getQualification1";
         qualificationUL.appendChild(qualificationLI);
     
         qualification = document.getElementById("qualification").value;
-        getQualification = document.getElementById("getQualification1").innerHTML = qualification;
-        
+        qualificationLI.innerHTML = qualification;
+    
+
+
         experienceUL = document.getElementById("experienceUL");
         experienceLI = document.createElement('li');
-        experienceLI.id = "getExperience1";
         experienceUL.appendChild(experienceLI);
-    
+
         experience = document.getElementById("experience").value;
-        getExperience = document.getElementById("getExperience1").innerHTML = experience;
-        
+        experienceLI.innerHTML = experience;
+    
+
+
         courseUL = document.getElementById("courseUL");
         courseLI = document.createElement('li');
-        courseLI.id = "getCourse1";
         courseUL.appendChild(courseLI);
-    
+
         course = document.getElementById("course").value;
-        getCourse = document.getElementById("getCourse1").innerHTML = course;
+        courseLI.innerHTML = course;
     
+
+
         skillsUL = document.getElementById("skillsUL");
         skillsLI = document.createElement('li');
-        skillsLI.id = "getSkills1";
         skillsUL.appendChild(skillsLI);
-    
+
         skills = document.getElementById("skills").value;
-        getSkills = document.getElementById("getSkills1").innerHTML = skills;
+        skillsLI.innerHTML = skills;
     }
 })
 
-let i = 1;
+
+
+let qi = 0;
 let addQualification = document.getElementById("addQualification");
-
-addQualification.addEventListener("click", () => {
-    qualification = document.getElementById("qualification").value;
-    if(qualification.trim() === ""){
-        alert("Please Fill Field")
-    }
-    else{
-        i++;
-        qualificationUL = document.getElementById("qualificationUL");
-        qualificationLI = document.createElement('li');
-        qualificationLI.id = "getQualification"+i;
-        qualificationUL.appendChild(qualificationLI);
-    
-        getQualification = document.getElementById("getQualification"+i).innerHTML = qualification;
-        qualification = document.getElementById("qualification").value = "";
-    }
+addQualification.addEventListener("click", function(){
+    qi++;
+    addMore("qualification","getQualification","Q", qi)
 })
 
+
+let ei = 0;
 let addExperience = document.getElementById("addExperience");
-
-addExperience.addEventListener("click", () => {
-    experience = document.getElementById("experience").value;
-    if(experience.trim() === ""){
-        alert("Please Fill Field")
-    }
-    else{
-        i++;
-        experienceUL = document.getElementById("experienceUL");
-        experienceLI = document.createElement('li');
-        experienceLI.id = "getExperience"+i;
-        experienceUL.appendChild(experienceLI);
-    
-        getExperience = document.getElementById("getExperience"+i).innerHTML = experience;
-        experience = document.getElementById("experience").value = "";
-    }
+addExperience.addEventListener("click", function() {
+    ei++;
+    addMore("experience", "getExpereince", "E", ei)
 })
 
+
+let ci = 0;
 let addCourse = document.getElementById("addCourse");
-
-addCourse.addEventListener("click", () => {
-    course = document.getElementById("course").value;
-    if(course.trim() === ""){
-        alert("Please Fill Field")
-    }
-    else{
-        i++;
-        courseUL = document.getElementById("courseUL");
-        courseLI = document.createElement('li');
-        courseLI.id = "getCourse"+i;
-        courseUL.appendChild(courseLI);
-    
-        getCourse = document.getElementById("getCourse"+i).innerHTML = course;
-        course = document.getElementById("course").value = "";
-    }
+addCourse.addEventListener("click", function() {
+    ci++;
+    addMore("course", "getCourse", "C", ci)
 })
 
+
+let si = 0;
 let addSkills = document.getElementById("addSkills");
+addSkills.addEventListener("click", function() {
+    si++;
+    addMore("skills", "getSkills", "S", si)
+})
 
-addSkills.addEventListener("click", () => {
-    skills = document.getElementById("skills").value;
-    if(skills.trim() === ""){
-        alert("Please Fill Field")
+
+
+
+
+function addMore(inputId,b,c,increament){
+    qualification = document.getElementById(inputId).value;
+    if(qualification.trim() === ""){
+        alert("Please Fill Out This Field")
     }
     else{
-        i++;
-        skillsUL = document.getElementById("skillsUL");
-        skillsLI = document.createElement('li');
-        skillsLI.id = "getSkills"+i;
-        skillsUL.appendChild(skillsLI);
+        appendQ = document.getElementById("append"+c)
+
+        paraBoxQ = document.createElement('div');
+        paraBoxQ.id = "paraBox"+c+increament;
+        paraBoxQ.classList.add("paraBox")
+
+        paraQ = document.createElement("p")
+        paraQ.id = "para"+c;
+
+        dlt = document.createElement("button")
+        dlt.id = "dlt"+c+increament;
+        dlt.classList.add("dlt")
+        dlt.innerHTML = "x";
+
+        paraBoxQ.appendChild(paraQ);
+        paraBoxQ.appendChild(dlt)
+
+        paraQ.innerHTML = qualification;
+        appendQ.appendChild(paraBoxQ)
+
+        ul = document.getElementById(inputId+"UL");
+        li = document.createElement("li");
+        li.id = c+"li"+increament;
+        li.innerHTML = qualification;
+        ul.appendChild(li)
+        
+        qualification = document.getElementById(inputId).value = "";
     
-        getSkills = document.getElementById("getSkills"+i).innerHTML = skills;
-        skills = document.getElementById("skills").value = "";
+        dlt.addEventListener("click", function(){
+            document.getElementById(c+"li"+increament).remove()
+            this.parentElement.remove();
+        })
     }
-})
+}
+
+
+
 
 
 let colorBtn = document.getElementById("setColor");
 
 colorBtn.addEventListener("click", () => {
     colorInput = document.getElementById("colorName").value;
-    if(colorInput.trim() === ""){
-        alert("Please Enter Color Name")
-    }
-    else{
-        document.getElementById("mainContainer").style.border = "15px solid "+colorInput;
 
-        border = document.querySelectorAll(".border");
-        border.forEach(border => {
-            border.style.border = "2px solid "+colorInput;
-        })
+    document.getElementById("mainContainer").style.border = "15px solid "+colorInput;
 
-        border1 = document.querySelectorAll(".border1");
-        border1.forEach(border1 => {
-            border1.style.border = "1px solid "+colorInput;
-        })
-        
-        colorInput = document.getElementById("colorName").value = "";
-    }
+    border = document.querySelectorAll(".border");
+    border.forEach(border => {
+        border.style.border = "2px solid "+colorInput;
+    })
+
+    border1 = document.querySelectorAll(".border1");
+    border1.forEach(border1 => {
+        border1.style.border = "1px solid "+colorInput;
+    })
 })
